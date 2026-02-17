bfeaaa76d9094a76a8cbfc5d072258864686a913
Maodan DESBLE <118923564+gomenasia@users.noreply.github.com>
2026-02-17 13:58:15 +0100

    debur auth responsive

---
+++ static/js/checkout_login_register.js
@@ -0,0 +1,13 @@
+function changePasswordHideVisible(Id, e) {
+  e.classList.toggle("fa-eye-slash");
+  let password = document.getElementById(Id);
+  password.setAttribute("type", password.getAttribute("type") === "password" ? "text" : "password");
+};
+
+// Permet de se concentrer automatiquement sur le champ email à l'ouverture de la page
+document.addEventListener('DOMContentLoaded', () => {
+    const emailInput = document.getElementById('email');
+    if (emailInput) {
+    emailInput.focus();
+    }
+});
