84659d3daef0678ae0d7f737bcf507e212481d04
Bleuenn Guilleux <bleuenn.guilleux@etudiant.univ-rennes.fr>
2026-02-21 15:20:48 +0100

    z'avias oublié

--- src/planning/__init__.py
+++ src/planning/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-plan_bp = Blueprint("planning", __name__, url_prefix="/plan", template_folder="templates")
+plan_bp = Blueprint("planning", __name__, url_prefix="/tasks", template_folder="templates")
 
 from . import routes  # noqa: F401, E402
