8808377bef65c892ad93fc7497dc3847858fbef1
gomenasia <118923564+gomenasia@users.noreply.github.com>
2026-02-25 00:32:54 +0100

    ressource tp

--- src/ressources/routes.py
+++ src/ressources/routes.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from . import ressources_bp
 
-@ressources_bp.route("/")
-def ressource_hub():
-    return render_template("ressources_hub.html")
+@ressources_bp.route("/tp")
+def ressource_tp():
+    return render_template("ressource_tp.html")
+
+@ressources_bp.route("/cm")
+def ressource_cm():
+    return render_template("ressource_cm.html")
+
+@ressources_bp.route("/projet")
+def ressource_projet():
+    return render_template("ressource_projet.html")
