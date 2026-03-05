5cd75847c084afd74ee47dea6500b7e46628267e
Maodan DESBLE <118923564+gomenasia@users.noreply.github.com>
2026-03-05 13:59:57 +0100

    visibiliter mdp

--- static/js/checkout_login_register.js
+++ static/js/checkout_login_register.js
@@ -1,5 +1,6 @@
 function changePasswordHideVisible(Id, e) {
-  e.classList.toggle("fa-eye-slash");  /*TODO*/
+  e.classList.toggle("fa-eye-slash"); 
+  e.classList.toggle("fa-eye"); 
   let password = document.getElementById(Id);
   password.setAttribute("type", password.getAttribute("type") === "password" ? "text" : "password");
 };
